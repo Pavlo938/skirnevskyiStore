2b80a5b31def7d32dde4f3cd358481082c6434f2
Pavlo938 <pavlo.skirnevskyi@gmail.com>
2019-01-16 09:41:42 +0200

    Adding bootstrap to the project. Configuring all settings

---
+++ src/main/webapp/WEB-INF/resources/js/controller.js
@@ -0,0 +1,43 @@
+/**
+ * Created by Yuth on 12/7/2016.
+ */
+var cartApp = angular.module ("cartApp", []);
+
+cartApp.controller("cartCtrl", function ($scope, $http) {
+
+    $scope.refreshCart = function () {
+        $http.get('/eCamStore/rest/cart/'+$scope.cartId).success(function (data) {
+            $scope.cart = data;
+        });
+    };
+
+    $scope.clearCart = function () {
+        $http.delete('/eCamStore/rest/cart/'+$scope.cartId).success($scope.refreshCart());
+    };
+
+    $scope.initCartId = function (cartId) {
+        $scope.cartId= cartId;
+        $scope.refreshCart(cartId);
+    };
+
+    $scope.addToCart = function (productId) {
+        $http.put('/eCamStore/rest/cart/add/'+productId).success(function () {
+           alert("Product successfully added to the cart!");
+        });
+    };
+
+    $scope.removeFromCart = function (productId) {
+        $http.put('/eCamStore/rest/cart/remove/'+productId).success(function (data) {
+            $scope.refreshCart();
+        });
+    };
+
+    $scope.calGrandTotal = function () {
+        var grandTotal=0;
+        for (var i=0; i<$scope.cart.cartItems.length; i++) {
+            grandTotal += $scope.cart.cartItems[i].totalPrice;
+        }
+
+        return grandTotal;
+    };
+});
